5159dda9887598438ef1a407ac27310f6d4e51c9
alex galaz <alejandrogalaz21@gmail.com>
2024-11-20 19:45:30 -0600

    print cards

--- main.go
+++ main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	cards := []string{"Ace of Diamonds", newCard()}
+
+	for i, card := range cards { 
+		fmt.Println(card + " " + string(i))
+	}
+}
+
+func newCard() string {
+	card := "Ace of Diamonds"
+	return card
 }
