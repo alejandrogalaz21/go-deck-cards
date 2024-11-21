f2f912265311563bf72b566f6228c6e17acf1899
alex galaz <alejandrogalaz21@gmail.com>
2024-11-20 21:11:14 -0600

    create a new deck method

--- deck.go
+++ deck.go
@@ -5,9 +5,23 @@ import "fmt"
 // deck type represents a slice of strings.
 type deck []string
 
+func newDeck() deck { 
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value + " of " + suit)
+		}
+	}
+
+	return cards
+}
+
 // print method prints all cards in the deck with their index.
 func (d deck) print() {
 	for i, card := range d {
-		fmt.Println(i, card)
+		fmt.Println(i+1, card)
 	}
 }
--- main.go
+++ main.go
@@ -3,15 +3,7 @@ package main
 // main is the entry point for the program.
 func main() {
 	// Create a new deck of cards with an initial card.
-	cards := deck{"Ace of Spaces"}
-	// Add a new card to the deck.
-	cards = append(cards, "Two of Hearts")
-	// Print all cards in the deck.
+	cards := newDeck()
 	cards.print()
 }
 
-// newCard creates and returns a new card as a string.
-func newCard() string {
-	card := "Ace of Diamonds"
-	return card
-}
